3c300f3d38124cf31b4c33745090bf5697b50534
lilliancoo86 <lilliancoo86@gmail.com>
2025-01-17 01:57:56 +0800

    使用 get_position 作为主要方法 使用 get_today_position 作为备用方法

--- trading/position_manager.py
+++ trading/position_manager.py
@@ -628,8 +628,8 @@ class DoomsdayPositionManager:
             positions_data = {"active": []}
             
             try:
-                # 使用 positions() 方法获取持仓
-                stock_positions = await self.trade_ctx.positions()
+                # 使用 get_position 方法获取持仓
+                stock_positions = await self.trade_ctx.get_position()
                 
                 if stock_positions:
                     for pos in stock_positions:
@@ -669,8 +669,8 @@ class DoomsdayPositionManager:
                 self.logger.info("尝试使用备用方法获取持仓...")
                 
                 try:
-                    # 尝试使用 account_positions() 方法
-                    stock_positions = await self.trade_ctx.account_positions()
+                    # 尝试使用 get_today_position 方法
+                    stock_positions = await self.trade_ctx.get_today_position()
                     if stock_positions:
                         for pos in stock_positions:
                             position_data = {
